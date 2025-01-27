5b239d077ccd57bd0b9d87265dfa80c73cbc2aa2
sunilsd7 <sumildumre555@gmail.com>
2025-01-27 10:30:49 +0545

    Updated responsive for all screen

--- src/components/SecondPart.jsx
+++ src/components/SecondPart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Overlay from './Overlay';
-import Button from './Button';
+import Buttonn from './Button';
 
 const SecondPart = () => {
   return (
@@ -14,7 +14,7 @@ const SecondPart = () => {
             quis nostrud exercitation ullamco laboris.
           </p>
           <div>
-            <Button
+            <Buttonn
               style="text-white bg-blue px-9 py-4 font-bold rounded-2xl hover:bg-dark-blue transition-colors duration-300"
               text="Learn More"
             />
